e2f2227a0a52995b345785fa4162e28fced6a2a2
avelycure <van.emelin2012@yandex.ru>
2022-03-19 00:40:24 +0300

    Added simple implementation of widget

--- widgets/src/main/java/com/example/widgets/MovieWidget.kt
+++ widgets/src/main/java/com/example/widgets/MovieWidget.kt
@@ -1,4 +1,25 @@
 package com.example.widgets
 
-class MovieWidget {
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
+import android.content.Context
+import android.widget.RemoteViews
+
+class MovieWidget: AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+
+        val thisWidget = ComponentName(context, MovieWidget::class.java)
+        val allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget)
+
+        for(widgetId in allWidgetIds){
+            val remoteViews = RemoteViews(context.packageName, R.layout.widget_layout)
+            appWidgetManager.updateAppWidget(widgetId, remoteViews)
+        }
+    }
 }
